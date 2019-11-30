9874c2013ae8abe6e624b0a8bda208235b538bb4
ext2 <ext2@qq.com>
2019-11-30 14:10:29 +0800

    Add 2 buttons of "Unload project" and "Reload project"

--- SolutionToolbarSetStartup/SetStartupButton.cs
+++ SolutionToolbarSetStartup/SetStartupButton.cs
@@ -20,7 +20,9 @@ namespace SolutionToolbarSetStartup
         /// <summary>
         /// Command ID.
         /// </summary>
-        public const int CommandId = 0x0100;
+        public const int CommandStartupId = 0x0100;
+        public const int CommandUnloadId = 0x0101;
+        public const int CommandReloadId = 0x0102;
 
         /// <summary>
         /// Command menu group (command set GUID).
@@ -49,9 +51,17 @@ namespace SolutionToolbarSetStartup
             OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
             if (commandService != null)
             {
-                var menuCommandID = new CommandID(CommandSet, CommandId);
-                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
-                commandService.AddCommand(menuItem);
+                var menuStartupCommandID = new CommandID(CommandSet, CommandStartupId);
+                var menuStartupItem = new MenuCommand(this.StartupMenuItemCallback, menuStartupCommandID);
+                commandService.AddCommand(menuStartupItem);
+
+                var menuUnloadCommandID = new CommandID(CommandSet, CommandUnloadId);
+                var menuUnloadItem = new MenuCommand(this.UnloadMenuItemCallback, menuUnloadCommandID);
+                commandService.AddCommand(menuUnloadItem);
+
+                var menuReloadCommandID = new CommandID(CommandSet, CommandReloadId);
+                var menuReloadItem = new MenuCommand(this.ReloadMenuItemCallback, menuReloadCommandID);
+                commandService.AddCommand(menuReloadItem);
             }
         }
 
@@ -91,7 +101,7 @@ namespace SolutionToolbarSetStartup
         /// </summary>
         /// <param name="sender">Event sender.</param>
         /// <param name="e">Event args.</param>
-        private void MenuItemCallback(object sender, EventArgs e)
+        private void StartupMenuItemCallback(object sender, EventArgs e)
         {
             // Ref: https://msdn.microsoft.com/en-us/library/bb166773.aspx?f=255&MSPPError=-2147217396
 
@@ -113,7 +123,11 @@ namespace SolutionToolbarSetStartup
             //    throw new Exception("Active Solution Projects [0] is null");
             //}
 
-            dte.ExecuteCommand("Project.SetasStartUpProject");
+            try
+            {
+                dte.ExecuteCommand("Project.SetasStartUpProject");
+            }
+            catch { }
 
             //string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
             //string title = "SetStartupButton";
@@ -126,6 +140,36 @@ namespace SolutionToolbarSetStartup
             //    OLEMSGICON.OLEMSGICON_INFO,
             //    OLEMSGBUTTON.OLEMSGBUTTON_OK,
             //    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+
+        private void UnloadMenuItemCallback(object sender, EventArgs e)
+        {
+            EnvDTE.DTE dte = (EnvDTE.DTE)Package.GetGlobalService(typeof(EnvDTE.DTE));
+            if (dte == null)
+            {
+                throw new Exception("Unable to retrieve DTE");
+            }
+
+            try
+            {
+                dte.ExecuteCommand("Project.UnloadProject");
+            }
+            catch { }
+        }
+
+        private void ReloadMenuItemCallback(object sender, EventArgs e)
+        {
+            EnvDTE.DTE dte = (EnvDTE.DTE)Package.GetGlobalService(typeof(EnvDTE.DTE));
+            if (dte == null)
+            {
+                throw new Exception("Unable to retrieve DTE");
+            }
+
+            try
+            {
+                dte.ExecuteCommand("Project.ReloadProject");
+            }
+            catch { }
         }
     }
 }
